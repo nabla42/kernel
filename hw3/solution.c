2c7c206c6fbc83066da6672a9a33882b7629af76
nabla42 <shurigina95@mail.ru>
2019-12-07 22:31:44 +0000

    Done

--- hw3/solution.c
+++ hw3/solution.c
@@ -8,6 +8,7 @@ extern ssize_t generate_output(int sum, short *arr, size_t size, char *buf);
 
 int init_module(void)
 {
+	CHECKER_MACRO;
 	int sum, i, calls;
 	calls = 13;
 	char buf[255];
@@ -32,13 +33,13 @@ short arr_value;
 			arr[j] = arr_value;
 		//	printk(KERN_INFO "%d", arr[j]);
 		}
-		//	printk(KERN_INFO "~~~~~~~~~~~~~~");
+
 		sum = array_sum(arr, size);
 		for (k=0; k<size; k++)
 		{
 			proof_sum += arr[k];
 		}
-		generate_output(int sum, short *arr, size_t size, char *buf);
+		generate_output(sum, arr, size, buf);
 
 		if (sum == proof_sum)
 			pr_info("[kernel_mooc]: %s", buf);
@@ -49,7 +50,9 @@ short arr_value;
 	return 0;
 }
 
-void cleanup_module(void)
+void exit_module(void)
 {
+	CHECKER_MACRO;
 	printk(KERN_INFO "Solution close!\n");
 }
+module_exit(exit_module);
